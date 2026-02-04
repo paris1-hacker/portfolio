c90b5b91a0ac20360e31d0b91d7e56e46bd190ae
ofem <ofempatrick10@gmail.com>
2026-02-04 14:37:09 -0800

    Update ALLOWED_HOSTS in settings.py for production deployment

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.portfolio-vert-xi-4fj2gfcr5q.vercel.app', 'portfolio-vert-xi-4fj2gfcr5q.vercel.app']
 
 
 # Application definition
