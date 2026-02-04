8fadca942657030dc543b814f1498c1d0d91997e
ofem <ofempatrick10@gmail.com>
2026-02-04 14:51:17 -0800

    Fix URL patterns in my_personal_web and update Vercel configuration for correct path

--- my_personal_web/urls.py
+++ my_personal_web/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.my_porfolio, name='portfolio'),
+    path('', views.my_porfolio, name='portfolio'),
     path('download-cv/', views.download_cv, name='download-cv'),
     path('contact-form/', views.contact_view, name='contact-form')
 ]
--- porfolio/urls.py
+++ porfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('my_personal_web.urls'))
+    path('', include('my_personal_web.urls'))
 ]
