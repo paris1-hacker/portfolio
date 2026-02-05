5c8d70e7df07f35d0c04c28edbc830cdd6d32263
ofem <ofempatrick10@gmail.com>
2026-02-04 16:04:25 -0800

    Update ALLOWED_HOSTS in settings.py for correct domain configuration

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.portfolio-vert-xi-4fj2gfcr5q.vercel.app', 'portfolio-vert-xi-4fj2gfcr5q.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.portfolio-rouge-chi-52.vercel.app', 'portfolio-rouge-chi-52.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
